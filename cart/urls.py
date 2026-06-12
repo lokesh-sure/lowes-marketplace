60893a74c46ae638b3c7153b02dcc98b4b5aff7f
lokesh-sure <surelokeshyadav@gmail.com>
2026-06-12 22:58:41 +0530

    Cart add to cart logic and URL routing

---
+++ cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import add_to_cart
+
+urlpatterns = [
+    path(
+        'add/<int:product_id>/',
+        add_to_cart,
+        name='add_to_cart'
+    ),
+]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Cart, CartItem
+from products.models import Product
+
+
+@login_required
+def add_to_cart(request, product_id):
+    cart = Cart.objects.get(user=request.user)
+
+    product = get_object_or_404(
+        Product,
+        id=product_id
+    )
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product
+    )
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('admin:index')
