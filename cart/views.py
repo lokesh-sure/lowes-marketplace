29ed8e9238785c2678ff83fba0ae80e18b2ae7d4
lokesh-sure <surelokeshyadav@gmail.com>
2026-06-15 22:14:06 +0530

    View cart, total calculation and remove cart item

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
 from .models import Cart, CartItem
@@ -24,3 +24,38 @@ def add_to_cart(request, product_id):
         cart_item.save()
 
     return redirect('admin:index')
+
+
+@login_required
+def view_cart(request):
+    cart = Cart.objects.get(user=request.user)
+
+    cart_items = CartItem.objects.filter(
+        cart=cart
+    )
+
+    total = 0
+
+    for item in cart_items:
+        total += item.product.price * item.quantity
+
+    return render(
+        request,
+        'cart/cart.html',
+        {
+            'cart_items': cart_items,
+            'total': total
+        }
+    )
+
+
+@login_required
+def remove_from_cart(request, item_id):
+    cart_item = get_object_or_404(
+        CartItem,
+        id=item_id
+    )
+
+    cart_item.delete()
+
+    return redirect('view_cart')
