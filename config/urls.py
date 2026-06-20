02852010df3a92233df95cd6ce1794d63374fd06
lokesh-sure <surelokeshyadav@gmail.com>
2026-06-20 23:51:05 +0530

    Added authentication and product search

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
 
     path(
         'accounts/',
         include('accounts.urls')
+    ),
+
+    path(
+        'products/',
+        include('products.urls')
     ),
 
     path(
--- products/views.py
+++ products/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+
+def product_list(request):
+
+    query = request.GET.get('q')
+
+    products = Product.objects.all()
+
+    if query:
+        products = Product.objects.filter(
+            name__icontains=query
+        )
+
+    return render(
+        request,
+        'products/product_list.html',
+        {
+            'products': products
+        }
+    )
