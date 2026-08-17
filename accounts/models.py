1765ee3a0a5cd4fed6e133c66a4fd4f202561e88
lokesh-sure <surelokeshyadav@gmail.com>
2026-08-17 23:23:51 +0530

    Complete Day 1 order history and order details

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class UserProfile(models.Model):
-    full_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15)
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        null=True,
+        blank=True
+    )
+
+    full_name = models.CharField(
+        max_length=100
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    phone = models.CharField(
+        max_length=15
+    )
 
     def __str__(self):
         return self.full_name
